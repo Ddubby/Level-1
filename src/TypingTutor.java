16c3dc6f4409066ece026b2fdcda4aef0ccb6b9d
GabrielZ1 <League@AdministorsiMac.attlocal.net>
2017-11-22 18:28:46 -0800

    Jukebox, Typing Tutor

---
+++ src/TypingTutor.java
@@ -0,0 +1,64 @@
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class TypingTutor implements KeyListener {
+private JFrame frame=new JFrame();
+private JLabel label=new JLabel();
+private Color red=new Color(255,0,0);
+private Color green=new Color(0,255,0);
+private char currentLetter=generateRandomLetter();
+
+public static void main(String[] args) {
+	new TypingTutor().start();
+}
+private void start() {
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+ frame.setTitle("Type or Die");
+ frame.setSize(350,350);
+ frame.add(label);
+ frame.addKeyListener(this);
+ label.setText(""+currentLetter);
+ label.setFont(label.getFont().deriveFont(78.0f));
+ label.setHorizontalAlignment(JLabel.CENTER);
+
+} 
+char generateRandomLetter() {
+	Random r = new Random();
+	return (char) (r.nextInt(26) + 'a');
+}
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	if (currentLetter==e.getKeyChar()) {
+		
+	System.out.println("Correct");
+label.setOpaque(true);
+	label.setBackground(green);
+	}
+	else {
+	label.setBackground(red);
+	}
+	frame.repaint();
+	}
+
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("You typed: "+e.getKeyChar());
+	
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	 currentLetter=generateRandomLetter();
+	label.setText(" "+currentLetter);
+}
+
+}
+
